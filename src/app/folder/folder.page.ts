1b392cb3edac662463ed1f6a5f67832709d41d95
c639969 <ssaifalis@bcbsm.com>
2024-05-20 11:55:51 -0400

    commit 2

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -14,4 +14,8 @@ export class FolderPage implements OnInit {
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
+  goToExpandableComponent() {
+    
+  }
 }
